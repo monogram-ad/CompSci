c8982e88730b86ae2fb9258514c50e0f5c7fcb18
Aditya Sharma <aditya.sharma.tech@outlook.com>
2026-02-03 00:37:04 +0530

    Refactor variable swapping and add additional logicmaking code with inclusion of extra variable

--- p3.c
+++ p3.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-
 int main() {
     int a = 45;
     int b = 41;
-
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    printf("first num: %d\n second num: %d\n", a, b);
     a=a+b;
     b=a-b;
     a=a-b;
-
     printf("first num: %d\n second num: %d", a, b);
-
     return 0;
 }
