eb1d97ec52ddcefd9ec1f8b77dbc3765832d2de2
Aditya Sharma <adityaisrollno21@gmail.com>
2026-02-03 11:20:03 +0530

    looping

--- 1.c
+++ 1.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 int main() {
 	int n =10, i=1, sum=0;
-	while(i<=n){
+	/*while(i<=n){
 		sum = sum +i;
 		i = i+1;
 	}
+	printf("sum = %d", sum);*/
+	
+	/*do{
+		sum = sum +i;
+		i = i+1;
+	} while(i<=n);
+	printf("sum = %d", sum);*/
+	
+	for(i=1;i<=n; i++){
+		sum +=i;
+	}
+	/*
+	for(;i<=n;){
+		sum +=i;
+		i++;
+	}
+	*/
+	
 	printf("sum = %d", sum);
+	
 	return 0;
 }
